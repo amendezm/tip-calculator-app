eb4aeae050344a4f33d9905d66a7a8be0277e109
cteyr123 <cteyr123@gmail.com>
2022-04-14 15:39:07 -0400

    efectos visuales adicionados

---
+++ js/index.js
@@ -0,0 +1,30 @@
+window.onload = () => {
+
+const buttons= document.querySelectorAll(".button");
+      buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            if (button.classList.contains("active")) {
+                button.classList.remove("active");
+              }else {
+                    buttons.forEach((button) => {
+                      button.classList.remove("active");
+                    });
+              }
+              button.classList.add("active");
+            //  console.log(document.getElementsByClassName("button"));
+
+        });
+        
+        const field=document.getElementById("bill").textContent;
+            
+        
+        
+        console.log(`Hola Mundo ${field}`);
+        
+
+
+      });
+       
+
+
+}
